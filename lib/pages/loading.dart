f54b6b02345c9eabfc225b1fd06bf3ec67dc7b8e
Tijil <tijil2008@gmail.com>
2021-08-06 13:44:25 +0530

    Made Loading Screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -32,9 +33,14 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-      )
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child:SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+
     );
   }
 }
