7082ffc34e4513fdca3f13f4b035a38ef552012d
Tijil <tijil2008@gmail.com>
2021-08-06 14:40:16 +0530

    Updating Time

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
 
@@ -22,6 +24,19 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+
+    // navigate to home screen
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +55,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
             child: Card(
               child: ListTile(
                 onTap: (){
-                  print(locations[index].location);
+                  updateTime(index);
                 },
                 title: Text(locations[index].location),
                 leading: CircleAvatar(
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,10 +10,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
 
-    Map? data = ModalRoute.of(context)!.settings.arguments as Map?;
+    data = (data!.isNotEmpty ? data :ModalRoute.of(context)!.settings.arguments as Map?)!;
     print(data);
 
     //set background
@@ -36,8 +38,16 @@ class _HomeState extends State<Home> {
             child: Column(
               children: <Widget>[
                 FlatButton.icon(
-                    onPressed:(){
-                      Navigator.pushNamed(context, '/location');
+                    onPressed:() async{
+                     dynamic result = await Navigator.pushNamed(context, '/location');
+                     setState(() {
+                       data = {
+                         'time': result['time'],
+                         'location': result['location'],
+                         'isDaytime': result['isDaytime'],
+                         'flag': result['flag'],
+                       };
+                     });
                     },
                     icon: Icon(
                         Icons.edit_location,
